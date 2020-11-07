6de92e8be4b293c3c6356fd5029aa9c9b8dd07ed
JD Martinez <focus4ursoul@gmail.com>
2020-11-07 14:19:19 -0800

    add routes for html files'

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// app.use(require())
+app.use(require("./routes/html-routes.js"));
+// app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
     console.log(`server is listening on http://localhost:${PORT}`);
